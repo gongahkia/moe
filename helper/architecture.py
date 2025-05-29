8467a09975748746888da456bca8f73d99f207fe
gongahkia <angryapplegravy@gmail.com>
2025-05-29 11:09:08 +0800

    feat(app): added notes

--- helper/architecture.py
+++ helper/architecture.py
@@ -1,8 +1,12 @@
+# ----- required imports -----
+
 from diagrams import Diagram, Cluster, Edge
 from diagrams.onprem.client import User
 from diagrams.programming.language import Python
 from diagrams.onprem.container import Docker
 from diagrams.custom import Custom
+
+# ----- execution code -----
 
 graph_attr = {
     "splines": "ortho",
@@ -16,7 +20,6 @@ with Diagram("Moe Architecture", show=True, direction="LR", graph_attr=graph_att
     user = User("User")
     discord_gateway = Custom("Discord Gateway", "./discord.png")
     steam_api = Custom("Steam API", "./steam.png")
-
     with Cluster("Docker Container"):
         docker = Docker("Docker Engine")
         with Cluster("Bot Services"):
@@ -25,8 +28,6 @@ with Diagram("Moe Architecture", show=True, direction="LR", graph_attr=graph_att
             python_bot - Edge(color="brown", style="dashed") - redis_cache
         docker - python_bot
         docker - redis_cache
-
-    # Use xlabel for all edges to keep labels off arrows
     user >> Edge(xlabel="1. /compare") >> discord_gateway
     discord_gateway >> Edge(xlabel="2. Process") >> python_bot
     python_bot >> Edge(xlabel="3. Check cache") >> redis_cache
